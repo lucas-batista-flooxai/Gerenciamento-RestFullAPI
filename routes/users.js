fd335f34c40c96260169cfdd7a90b254a408ea99
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-29 14:53:36 -0300

    feat: rotes post, put and delete with restify

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,32 @@ router.get("/", function(req, res, next) {
   client.get("/users", function (err, request, response, obj) {
     assert.ifError(err);
 
-    res.end(JSON.stringify(obj, null, 2));
+    res.end(obj);
   });
-});           
+});
+
+router.put("/:id", function(req, res, next) {
+  client.put(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err);
+
+    res.end(obj);
+  });
+}); 
+
+router.delete("/:id", function(req, res, next) {
+  client.delete(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err);
+
+    res.end(obj);
+  });
+}); 
+
+router.post("/:id", function(req, res, next) {
+  client.post(`/users`, req.body, function (err, request, response, obj) {
+    assert.ifError(err);
+
+    res.end(obj);
+  });
+}); 
 
 module.exports = router;
