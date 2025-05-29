555a7cd708010e649ee3f795dafac491490b0fd8
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-29 13:35:58 -0300

    feat: usign restify for acess RestAPI

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
+var assert = require("assert");
 var router = express.Router();
+var restify = require("restify-clients");
+
+var client = restify.createJsonClient({
+  url: "http://localhost:4000/",
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  client.get("/users", function (err, request, response, obj) {
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+  });
 });
 
 module.exports = router;
